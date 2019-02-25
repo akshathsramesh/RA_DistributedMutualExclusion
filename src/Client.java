1b38dd6927a6e7d6ccc825005f62beb5f1ddd17a
akshathsramesh <akshath.sramesh@gmail.com>
2019-02-24 23:52:30 -0600

    added capability to parse server nodes

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.lang.reflect.Array;
-import java.net.InetAddress;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -11,8 +9,7 @@ public class Client {
 
     String Id;
     List<Node> allClientNodes = new LinkedList<>();
-    List<Node> allServerList = new LinkedList<>();
-
+    List<Node> allServerNodes = new LinkedList<>();
     Integer logicalClock = 0;
 
     public Client(String id) {
@@ -36,6 +33,15 @@ public class Client {
         this.allClientNodes = allClientNodes;
     }
 
+
+    public List<Node> getAllServerNodes() {
+        return allServerNodes;
+    }
+
+    public void setAllServerNodes(List<Node> allServerNodes) {
+        this.allServerNodes = allServerNodes;
+    }
+
     public Integer getLogicalClock() {
         return logicalClock;
     }
@@ -47,20 +53,7 @@ public class Client {
 
 
 
-
-
-
-    public static void main(String[] args) {
-
-
-//        check for arguments and use the client id to start the client node
-
-
-        Client C1 = new Client("1");
-
-
-        System.out.println("Starting Client with ID: " + C1.getId());
-
+    public void setClientList(){
         try {
             BufferedReader br = new BufferedReader(new FileReader("ClientAddressAndPorts.txt"));
             try {
@@ -69,23 +62,63 @@ public class Client {
 
                 while (line != null) {
                     sb.append(line);
-                    List<String> parsed = Arrays.asList(line.split(","));
-                    System.out.println(parsed);
-                    Node n1 = new Node(parsed.get(0),parsed.get(1),parsed.get(2));
-                    C1.getAllClientNodes().add(n1);
+                    List<String> parsed_client = Arrays.asList(line.split(","));
+                    Node n1 = new Node(parsed_client.get(0),parsed_client.get(1),parsed_client.get(2));
+                    this.getAllClientNodes().add(n1);
                     sb.append(System.lineSeparator());
                     line = br.readLine();
                 }
                 String everything = sb.toString();
                 System.out.println(everything);
-                System.out.println(C1.getAllClientNodes().size());
+                System.out.println(this.getAllClientNodes().size());
 
             } finally {
                 br.close();
             }
         }
-        catch (Exception e){
-
+        catch (Exception e) {
         }
+    }
+
+
+    public void setServerList(){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("ServerAddressAndPorts.txt"));
+            try {
+                StringBuilder sb = new StringBuilder();
+                String line = br.readLine();
+
+                while (line != null) {
+                    sb.append(line);
+                    List<String> parsed_server = Arrays.asList(line.split(","));
+                    Node n_server = new Node(parsed_server.get(0),parsed_server.get(1),parsed_server.get(2));
+                    this.getAllServerNodes().add(n_server);
+                    sb.append(System.lineSeparator());
+                    line = br.readLine();
+                }
+                String everything = sb.toString();
+                System.out.println(everything);
+                System.out.println(this.getAllServerNodes().size());
+
+            } finally {
+                br.close();
+            }
+        }
+        catch (Exception e) {
+        }
+
+    }
+    public static void main(String[] args) {
+
+
+//        check for arguments and use the client id to start the client node
+
+
+        Client C1 = new Client("1");
+        C1.setClientList();
+        C1.setServerList();
+        System.out.println("Starting Client with ID: " + C1.getId());
+
+
     }
 }

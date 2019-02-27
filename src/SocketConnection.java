aabb20f88e49d4c3219748e40c60f2ab157cfb87
akshathsramesh <akshath.sramesh@gmail.com>
2019-02-26 20:19:53 -0600

    added mesh connection logic

---
+++ src/SocketConnection.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class SocketConnection {
+
+
+    Socket otherClient;
+    String my_id;
+    String remote_id;
+    BufferedReader in;
+    PrintWriter out;
+    Boolean Initiator;
+
+    public String getRemote_id() {
+        return remote_id;
+    }
+
+    public void setRemote_id(String remote_id) {
+        this.remote_id = remote_id;
+    }
+
+    public SocketConnection(Socket otherClient, String myId, Boolean Initiator) {
+        this.otherClient = otherClient;
+        this.my_id = myId;
+        try{
+           in = new BufferedReader(new InputStreamReader(this.otherClient.getInputStream()));
+           out = new PrintWriter(this.otherClient.getOutputStream(), true);
+        }
+        catch (Exception e){
+
+        }
+
+        try {
+            if(!Initiator) {
+                out.println("SEND_ID");
+                System.out.println("SEND_ID request sent");
+                remote_id = in.readLine();
+                System.out.println("SEND_ID request response recieved with ID: " + remote_id);
+            }
+        }
+
+        catch (Exception e){
+
+        }
+        Thread read = new Thread(){
+            public void run(){
+                while(rx_cmd(in,out) != 0) { }
+            }
+        };
+        read.setDaemon(true); 	// terminate when main ends
+        read.start();
+    }
+
+
+    public int rx_cmd(BufferedReader cmd,PrintWriter out) {
+        try {
+            String cmd_in = cmd.readLine();
+            if(cmd_in.equals("P")){
+                System.out.println("P recieved from sender");
+            }
+
+            else if(cmd_in.equals("SEND_ID")){
+                out.println(this.my_id);
+            }
+        }
+        catch (Exception e){}
+        return 1;
+    }
+
+
+    public void publish() {
+        out.println("P");
+    }
+
+    public Socket getOtherClient() {
+        return otherClient;
+    }
+
+    public void setOtherClient(Socket otherClient) {
+        this.otherClient = otherClient;
+    }
+}
+
+
